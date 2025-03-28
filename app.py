88996df153db5887b01916b9cfecd816daa244c0
HanSolo324 <han324solo@gmail.com>
2025-03-28 16:11:48 +0900

    fix bug

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-import torch
-from torchvision import transforms, models 
+#import torch
+#from torchvision import transforms, models 
 from PIL import Image
 import os
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 # PyTorch 모델 로드 (예: 학습된 모델 파일 'model.pt' 사용)
 #model = torch.load('model.pt', map_location=torch.device('cpu'))  # 학습한 모델 파일 경로
+'''
 model = models.vgg16(pretrained=True)  # 'pretrained=True'는 사전 학습된 가중치 사용
 model.eval()  # 평가 모드로 전환
 
@@ -17,7 +18,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-
+'''
 # 클래스 이름 (예시, 학습한 모델에 맞게 수정)
 class_names = ['고양이', '강아지', '새']  # 네 모델의 출력 클래스
 
@@ -34,17 +35,17 @@ def predict():
     image_path = os.path.join('static', image_name)
 
     # 이미지 로드 및 전처리
-    image = Image.open(image_path).convert('RGB')
-    image_tensor = transform(image).unsqueeze(0)  # 배치 차원 추가
+    #image = Image.open(image_path).convert('RGB')
+    #image_tensor = transform(image).unsqueeze(0)  # 배치 차원 추가
 
     # 모델 예측
-    with torch.no_grad():
-        output = model(image_tensor)
-        _, predicted = torch.max(output, 1)
-        #result = class_names[predicted.item()]
-        result = predicted.item()
-
+    #with torch.no_grad():
+    #    output = model(image_tensor)
+    #    _, predicted = torch.max(output, 1)
+    #    #result = class_names[predicted.item()]
+    #    result = predicted.item()
+    result = 'image_name'
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=55051)
+    app.run(debug=True, host='0.0.0.0', port=10000)
